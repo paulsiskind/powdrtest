ffc69d5d277df38ac25fc6aa5532b8806e195279
paulsiskind <paulsiskind@gmail.com>
2015-11-16 21:06:44 -0700

    initializing a test repo

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -22,35 +22,45 @@ var app = angular.module("theRiver", ['ngRoute'])
     
   })
   app.controller('HomeController', function($scope, $http){
-       $http.get('http://waterservices.usgs.gov/nwis/iv/?format=waterml,1.1&sites=09058000&parameterCd=00060,00065').then(function(response){
-      console.log(response)
-      $scope.flows = response.data
-    })
   })
   app.controller('RiverController', function($scope, $http){
+
     $scope.state = function(choice){
       console.log(choice)
-      if(choice === 'al'){
-        $scope.viewBarState = 'Alabama'
-        console.log($scope.viewBarState)
+      if(choice === 'vail'){
+        $scope.viewBarState = "Vail"
+        $scope.snotel = "842:CO"
       }
-      if(choice === 'ak'){
-        $scope.viewBarState = "Alaska"
+      if(choice === 'snowmass'){
+        $scope.viewBarState = 'Snowmass'
+        $scope.snotel = "542:CO"
       }
-      if(choice === 'az'){
-        $scope.viewBarState = 'Arizona'
+      if(choice === 'telluride'){
+        $scope.viewBarState = 'Telluride'
+        $scope.snotel = "713:CO"
       }
-      if(choice === 'ar'){
-        $scope.viewBarState = 'Arkansas'
+      if(choice === 'keystone'){
+        $scope.viewBarState = "Keystone"
+        $scope.snotel = "505:CO"
       }
-      if(choice === 'ca'){
-        $scope.viewBarState = "California"
+      if(choice === 'breckenridge'){
+        $scope.viewBarState = "breckenridge"
+        $scope.snotel = "531:CO"
       }
-      if(choice === 'co'){
-        $scope.viewBarState = "Colorado"
-      }
-      $scope.choice = choice;
+       $http.get("http://api.worldweatheronline.com/free/v2/ski.ashx?key=36ac695418a963238c6e44746bf57&q="+ $scope.viewBarState +"+co&format=json").then(function(response){
+      console.log(response.data.data)
+      $scope.weather = response.data.data.weather[0]
+      $scope.site = response.data.data.request[0]
+    })
+      $.getJSON('http://cors.io/?u=http://api.powderlin.es/station/'+ $scope.snotel +':SNTL?days=7',function(response){
+        console.log(response.data[7]);
+        $scope.snotelData = response.data[7]
+      }) 
+      
     }
+  })
+  app.controller('MapsController', function($scope, $http, ngMap){
+    
   })
     app.config(function($routeProvider, $locationProvider) {
     $routeProvider
@@ -61,6 +71,10 @@ var app = angular.module("theRiver", ['ngRoute'])
       .when('/rivers',{
         templateUrl: 'partials/rivers.html',
         controller: "RiverController"
+      })
+      .when('/maps',{
+        templateUrl: "partials/maps.html",
+        controller: "MapsController"
       })
       .otherwise( {redirectTo: '/'
       })
